96e11e2d7fbcdc0675cc8210c7b70c83de59cc2b
rizkynova <rizkynovayandiofficial@gmail.com>
2020-04-15 05:29:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tugas2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tugas2;
+/*
+     Tugas 2
+     Nama  : Rizky Novayandi
+     Kelas : IF4
+     NIM   : 10117140
+     Tanggal Pengerjaan : Rabu 15 April 2020 11:26 WIB
+*/
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
