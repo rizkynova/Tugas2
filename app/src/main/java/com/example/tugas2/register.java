33ebde84e0aa265899b73cca6deb654c40d01604
rizkynova <rizkynovayandiofficial@gmail.com>
2020-04-15 16:48:25 +0100

    Rehat. googling solusi background action bar
    Last commit 15 April 2020
    STUCK DI UBAH BACKGROUND Header

--- app/src/main/java/com/example/tugas2/register.java
+++ app/src/main/java/com/example/tugas2/register.java
@@ -2,6 +2,7 @@ package com.example.tugas2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 public class register extends AppCompatActivity {
@@ -9,7 +10,6 @@ public class register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
         getSupportActionBar().setTitle("Register");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_header);
