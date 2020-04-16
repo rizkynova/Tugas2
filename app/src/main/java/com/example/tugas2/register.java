e2423d1ba4a9dc57b6ebed314342d656c7ec1aea
rizkynova <rizkynovayandiofficial@gmail.com>
2020-04-16 06:33:44 +0100

    COMMIT REGISTER, HEADER UDAH KETEMU!

--- app/src/main/java/com/example/tugas2/register.java
+++ app/src/main/java/com/example/tugas2/register.java
@@ -1,19 +1,30 @@
 package com.example.tugas2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class register extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setTitle("Register");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.ic_header);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_register);
+
+        //toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Register");
+        setSupportActionBar(toolbar);
+
+        //set icon ke toolbar
+        toolbar.setNavigationIcon(R.drawable.ic_header_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
