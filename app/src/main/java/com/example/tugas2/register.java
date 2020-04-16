1880c9295e63edce1677f33380efde90ec6725a6
rizkynova <rizkynovayandiofficial@gmail.com>
2020-04-16 17:15:14 +0100

    Commit Verify beres,

--- app/src/main/java/com/example/tugas2/register.java
+++ app/src/main/java/com/example/tugas2/register.java
@@ -1,12 +1,25 @@
 package com.example.tugas2;
 
+/*
+     Tugas 2
+     Nama  : Rizky Novayandi
+     Kelas : IF4
+     NIM   : 10117140
+     Tanggal Pengerjaan : Kamis 15 April 2020 07:26 WIB
+*/
+
+
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,6 +29,7 @@ import org.w3c.dom.Text;
 
 public class register extends AppCompatActivity {
     EditText bloodtipe;
+    Button button;
     Toolbar toolbar;
     private Spinner spinnerz;
     @Override
@@ -33,6 +47,16 @@ public class register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        //pindah layar intent ke verify
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(register.this,verify.class);
+                startActivity(i);
             }
         });
     }
---
+++ app/src/main/java/com/example/tugas2/verify.java
@@ -0,0 +1,22 @@
+package com.example.tugas2;
+
+/*
+     Tugas 2
+     Nama  : Rizky Novayandi
+     Kelas : IF4
+     NIM   : 10117140
+     Tanggal Pengerjaan : Kamis 16 April 2020 23:06 WIB
+*/
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
