5752ef29f393c60fb07d6382b56897590f77c37f
rizkynova <rizkynovayandiofficial@gmail.com>
2020-04-16 17:47:25 +0100

    Commit Verifyaccount beres, last 1 tampilan lagi

--- app/src/main/java/com/example/tugas2/verifyaccount.java
+++ app/src/main/java/com/example/tugas2/verifyaccount.java
@@ -1,13 +1,5 @@
 package com.example.tugas2;
 
-/*
-     Tugas 2
-     Nama  : Rizky Novayandi
-     Kelas : IF4
-     NIM   : 10117140
-     Tanggal Pengerjaan : Kamis 16 April 2020 23:06 WIB
-*/
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,17 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class verify extends AppCompatActivity {
-    Button button;
+public class verifyaccount extends AppCompatActivity {
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify);
-        button = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_verifyaccount);
+        button = (Button)findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(verify.this,verifyaccount.class);
+                Intent i = new Intent(verifyaccount.this,register.class);
                 startActivity(i);
             }
         });
